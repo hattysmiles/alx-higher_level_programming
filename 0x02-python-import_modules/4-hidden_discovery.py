3bf759cf834deb910db71bc144265e72e986da6a
hattysmiles <106765765+hattysmiles@users.noreply.github.com>
2022-09-26 19:05:06 +0100

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import hidden_4
-for item in dir(hidden_4):
-    if (item[0] != '_'):
-        print(item)
+import sys
+import hidden_4 as hidden
+
+if __name__ != "__main__":
+    exit()
+
+for name in dir(hidden):
+    if name[0:2] != "__":
+        print(name)
