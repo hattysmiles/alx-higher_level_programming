cacebae31d62ced6f7fe6fead356496f78826890
hattysmiles <106765765+hattysmiles@users.noreply.github.com>
2022-10-10 21:08:05 +0100

    Update 102-magic_calculation.py

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -4,7 +4,7 @@ def magic_calculation(a, b):
     for i in range(1, 3):
         try:
             if (i > a):
-                raise Exception('Too far');
+                raise Exception('Too far')
             else:
                 var += (a**b)/i
         except:
