50c07276ba0d0a131c014976d92c5b0060a68cf4
hattysmiles <106765765+hattysmiles@users.noreply.github.com>
2022-10-19 22:28:00 +0100

    Update 0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
-"""Module for function that adds 2 integers or floats
-"""
 
 
 def add_integer(a, b=98):
-    """This is the add_integer function
-    Is to add 2 integers/floats
-    Args:
-      a (int or float): first number
-      b (int or float): second number
     """
-    if type(a) is not int and type(a) is not float:
-        raise TypeError("a must be an integer")
-    if type(b) is not int and type(b) is not float:
-        raise TypeError("b must be an integer")
-    if type(a) is float:
-        a = int(a)
-    if type(b) is float:
-        b = int(b)
+    adds an integer
+    unit tests located in tests/0-add_integer.txt
+    checks for type errors
+    """
+    if not isinstance(a, int):
+        if isinstance(a, float):
+            a = int(a)
+        else:
+            raise TypeError("a must be an integer")
+    if not isinstance(b, int):
+        if isinstance(b, float):
+            b = int(b)
+        else:
+            raise TypeError("b must be an integer")
     return a + b
