c724821ac24056e532f51836d4a20f2212ddd319
hattysmiles <106765765+hattysmiles@users.noreply.github.com>
2022-10-21 22:18:09 +0100

    Update 0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python3
+"""
+add_integer:
+    Checks if parameters are int
+    Returns sum of parameters
+"""
 
 
 def add_integer(a, b=98):
     """
-    adds an integer
-    unit tests located in tests/0-add_integer.txt
-    checks for type errors
+    Checks if int, otherwise return sum
     """
-    if not isinstance(a, int):
-        if isinstance(a, float):
-            a = int(a)
-        else:
-            raise TypeError("a must be an integer")
-    if not isinstance(b, int):
-        if isinstance(b, float):
-            b = int(b)
-        else:
-            raise TypeError("b must be an integer")
-    return a + b
+    if type(a) == float or type(b) == float:
+        a = int(a)
+        b = int(b)
+
+    if type(a) != int:
+        raise TypeError("a must be an integer")
+    elif type(b) != int:
+        raise TypeError("b must be an integer")
+    else:
+        return a + b
