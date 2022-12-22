16c74a2d12216e4abbaa3a55cdaf855276e60e1f
hattysmiles <106765765+hattysmiles@users.noreply.github.com>
2022-12-23 00:22:02 +0100

    Update model_state.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-"""script for using sqlalchemy to model our models using ORM
 """
+contains the class definition of a State and an instance Base
+"""
+
+import sqlalchemy
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -8,10 +11,7 @@ Base = declarative_base()
 
 
 class State(Base):
-    """state class for use with sqlalchemy
-        -> inherits from sqlalchemy declarative_base
-    """
+    """Representation of a state"""
     __tablename__ = 'states'
-
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
