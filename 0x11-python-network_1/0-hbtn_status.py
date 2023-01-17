9bc2153e185183b92e71d2bf7dc6fff341727abc
hattysmiles <106765765+hattysmiles@users.noreply.github.com>
2023-01-17 22:45:31 +0100

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
-"""script for testing status of web pages
-"""
+"""Fetches https://intranet.hbtn.io/status"""
+import urllib.request
+
+
+def fetcher():
+    """fetcher"""
+    with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
+        html = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode("utf-8")))
+
 if __name__ == "__main__":
-    import urllib.request
-    url = "https://intranet.hbtn.io/status"
-    with urllib.request.urlopen(url) as response:
-        bytes_content = response.read()
-        content = bytes_content.decode('utf-8')
-        print_str = '''Body response:
-\t- type: {}
-\t- content: {}
-\t- utf8 content: {}'''.format(type(bytes_content), bytes_content, content)
-        print(print_str)
+    fetcher()
