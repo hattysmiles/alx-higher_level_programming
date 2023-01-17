9e0a6670b89c37a5bf27caa320e0204a0f9c1575
hattysmiles <106765765+hattysmiles@users.noreply.github.com>
2023-01-17 23:45:22 +0100

    Update 5-hbtn_header.py

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""script for testing status of web pages
-"""
+"""Python script that takes in a URL, sends
+a request to the URL and displays the value of
+the variable X-Request-Id in the response header."""
+
+import requests
+from sys import argv
+
 if __name__ == "__main__":
-    import requests
-    import sys
-    url = sys.argv[1]
-    response = requests.get(url)
-    meta = response.headers
-    print(meta.get('X-Request-Id'))
+    print(requests.get(argv[1]).headers.get("X-Request-Id"))
